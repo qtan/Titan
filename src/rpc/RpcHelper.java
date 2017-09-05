dd47e6eddc1af46396421c8bdba292aae68fb9e7
Qi Tan <qtan@ualberta.ca>
2017-09-05 11:26:51 -0600

    dbconnection

---
+++ src/rpc/RpcHelper.java
@@ -0,0 +1,54 @@
+package rpc;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class RpcHelper {
+	//Parses a JSONObject from http request.
+	public static JSONObject readJsonObject(HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine())!=null) {
+				sb.append(line);
+			}
+			reader.close();
+			return new JSONObject (sb.toString());
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	// writes a JSONNObject to http response.
+	public static void writeJsonObject(HttpServletResponse response, JSONObject obj) {
+		try {
+			response.setContentType("application/jason");
+			PrintWriter out = response.getWriter();
+			out.print(obj);
+			out.flush();
+			out.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	//writes a jsonarray to http response.
+	public static void writeJsonArray (HttpServletResponse response,JSONArray array) {
+		try {
+			response.setContentType("application/jason");
+			PrintWriter out = response.getWriter();
+			out.print(array);
+			out.flush();
+			out.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/rpc/SearchItem.java
@@ -0,0 +1,116 @@
+package rpc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import entity.Item;
+import external.ExternalAPI;
+import external.ExternalAPIFactory;
+
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/*  PrintWriter out = response.getWriter();
+		if(request.getParameter("username")!=null) {
+			String username = request.getParameter("username");
+			out.print("Hello "+username);
+		}
+		out.flush();
+		out.close();
+	}
+	*/
+	/*
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1> This is a HTML page</h1>");
+		out.println("</body></html>");
+		out.flush();
+		out.close();
+	}
+	*/
+	/*
+		response.setContentType("application/json");
+		String username = "";
+		if(request.getParameter("username")!=null) {
+			username = request.getParameter("username");
+		}
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("username", username);
+		}catch (JSONException e) {
+			e.printStackTrace();
+		}
+		PrintWriter out = response.getWriter();
+		out.print(obj);
+		out.flush();
+		out.close();
+	}
+	*/
+	/*
+		JSONArray array = new JSONArray();
+		try {
+			array.put(new JSONObject().put("username", "abcd"));
+		}catch (JSONException e) {
+			e.printStackTrace();
+		}
+		RpcHelper.writeJsonArray(response, array);
+	}
+	*/
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		// Term can be empty or null.
+		String term = request.getParameter("term");
+		ExternalAPI externalAPI = ExternalAPIFactory.getExternalAPI();
+		List<Item> items = externalAPI.search(lat, lon, term);
+		List<JSONObject> list = new ArrayList<>();
+		try {
+			for (Item item : items) {
+				// Add a thin version of item object
+				JSONObject obj = item.toJSONObject();
+				list.add(obj);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray array = new JSONArray(list);
+		RpcHelper.writeJsonArray(response, array);
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
